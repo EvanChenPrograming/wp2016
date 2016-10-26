a2a6a5a714e95847c2eb164a7915d0bd197a1869
EvanChen <h34041092@ncku.edu.tw>
2016-10-26 14:20:30 +0800

    js working

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 
-//app.js
-
-  show(){
-    document.getElementById("show").innerHTML="押しちゃダメ！！";
-  }
-  greet(){
+function  greet(){
     alert("Hello world!");
   }
-  var x=false;
-meClicked(){
+
+function  show(){
+    document.getElementById("show").innerHTML="押しちゃダメ！！";
+  }
+
+var x=true;
+function meClicked(){
   if(x==false)document.getElementById("show").style.display="block";
   else document.getElementById("show").style.display="none";
+  x=!x;
 }
